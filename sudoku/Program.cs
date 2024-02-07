18de6f404f272feba9b9ba9ac01e04b070ad87a5
LiamBenZaken <liambz3210@gmail.com>
2024-02-07 12:27:10 +0200

    Add project files.

---
+++ sudoku/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //start the project
+        }
+    }
+}
